82af92b6db52c4131f7c5a70d5d18835da63b36b
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-08-29 07:40:17 +0530

    main

---
+++ src/main/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author chandrabanu
+ */
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        Class.forName("com.mysql.jdbc.Driver");
+        
+        Parent root = FXMLLoader.load(getClass().getResource("/view/fxml.fxml"));
+        Scene mainScene = new Scene(root);
+        
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Sunday Class");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+    }
+}
